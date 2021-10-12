d2df1c9959271bef0079476770209ea6e52449b4
ioanagh27 <86732665+ioanagh27@users.noreply.github.com>
2021-10-12 21:16:36 +0100

    Add all files

---
+++ script.js
@@ -0,0 +1,12 @@
+onmouseover="this.src='./resources/IMG_20181103_175816 (3).jpg'";
+onmouseout="this.src='./resources/IMG_20181103_175816 (2).jpg'";
+
+
+for(var i=1; i<2; i++) {
+    setInterval((function(me) {
+        return function() {
+            document.getElementById('me').style.transitionDuration='2s';
+            document.getElementById('me').style.transform='rotate(360deg)';
+        };
+    }(i)),100);
+}
