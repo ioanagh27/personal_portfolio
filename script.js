61600afa5bb96bdfbbdfbfad0f0ab75aa4869ba1
ioanagh27 <86732665+ioanagh27@users.noreply.github.com>
2021-10-24 21:50:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ onmouseout="this.src='./resources/IMG_20181103_175816 (2).jpg'";
 
 
 for(var i=1; i<2; i++) {
-    setInterval((function(me) {
+    setTimeOut((function(me) {
         return function() {
             document.getElementById('me').style.transitionDuration='2s';
             document.getElementById('me').style.transform='rotate(360deg)';
